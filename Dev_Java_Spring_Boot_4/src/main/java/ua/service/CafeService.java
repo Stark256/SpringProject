7e7160b549cf3146a750391b848038021afe2b63
ihork <ihork@DESKTOP-R5L0CG0>
2017-08-31 16:48:02 +0300

    Complete cafe

--- Dev_Java_Spring_Boot_4/src/main/java/ua/service/CafeService.java
+++ Dev_Java_Spring_Boot_4/src/main/java/ua/service/CafeService.java
@@ -3,7 +3,6 @@ package ua.service;
 import java.time.LocalTime;
 import java.util.List;
 
-import ua.entity.Cafe;
 import ua.entity.Type;
 import ua.model.request.CafeRequest;
 import ua.model.view.CafeView;
